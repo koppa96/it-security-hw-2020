ad2a5f082d6b144bc35b42faa7c351b5ed2321bd
Kókai Kristóf <archetiz@sch.bme.hu>
2020-10-26 01:49:56 +0100

    Adding CAFF file read

--- parser/parser_test/parser_test.cpp
+++ parser/parser_test/parser_test.cpp
@@ -1,15 +1,63 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 #include "LibApi.h"
+
+//Function to preview CAFF file content, only used for testing purposes
+void readAnim() {
+    int buff_len = 1;
+    //char* fbuff = new char[buff_len];
+    std::ifstream anim_in;
+    anim_in.open("..\\..\\files\\1.caff", std::ios_base::in | std::ios_base::binary);
+    char c;
+    bool prev_letter = false;
+    for (int i = 0; i < 128; i++) {
+        //anim_in.read(fbuff, buff_len);
+        //std::cout << fbuff;
+        anim_in.get(c);
+        if (c >= 'A' && c <= 'z') {
+            std::cout << c;
+            prev_letter = true;
+        }
+        else {
+            if (prev_letter)
+                std::cout << " " << (int)c << " ";
+            else
+                std::cout << (int)c << " ";
+            prev_letter = false;
+        }
+    }
+    anim_in.close();
+}
+
+void readAndparseAnim() {
+    //Reading whole file at once
+    std::ifstream anim_in("..\\..\\files\\1.caff", std::ios_base::in | std::ios_base::binary);
+    std::string anim_str{ std::istreambuf_iterator<char>(anim_in), std::istreambuf_iterator<char>() };
+    //Converting to c-style string to pass for parsing
+    const char* in_buff = anim_str.c_str();
+    //Setting output buffers and lengths
+    const int in_len = anim_str.length() + 1;
+    const int out_len = in_len;
+    char* out_buff = new char[out_len];
+    //std::cout.write(in_buff, 100 /*in_len - 1*/);
+    //Passing input for parsing
+    ParseAnimation(in_buff, in_len, out_buff, out_len);
+    //Releasing resources
+    anim_in.close();
+    delete[] out_buff;
+}
 
 int main()
 {
-    std::string in_txt = "AnotherTestString";
-    const int in_len = in_txt.length() + 1;
-    const char* in_buff = in_txt.c_str();
-    const int out_len = in_len;
-    char* out_buff = new char[out_len];
-    ParseAnimation(in_buff, in_len, out_buff, out_len);
-    std::cout << out_buff;
-    delete[] out_buff;
+    //readAnim();
+    readAndparseAnim();
+    //std::string in_txt = "AnotherTestString";
+    //const int in_len = in_txt.length() + 1;
+    //const char* in_buff = in_txt.c_str();
+    //const int out_len = in_len;
+    //char* out_buff = new char[out_len];
+    //ParseAnimation(in_buff, in_len, out_buff, out_len)
+    //std::cout << out_buff;
+    //delete[] out_buff;
 }
