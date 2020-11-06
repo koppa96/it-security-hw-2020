6efce6b3b26800a4c3088d8510fc605cd4079ae6
Kókai Kristóf <archetiz@sch.bme.hu>
2020-11-06 23:50:28 +0100

    Updating C# example

--- parser/parser_test_cs/Program.cs
+++ parser/parser_test_cs/Program.cs
@@ -1,20 +1,20 @@
 ﻿using System;
 using System.Text;
 using System.Runtime.InteropServices;
+using System.IO;
 
 namespace parser_test_cs
 {
 	class Program
 	{
 		[DllImport(@"..\parser_core.dll")]
-		private static extern int ParseAnimation(byte[] in_buffer, int in_len, byte[] out_buffer, int out_len);
+		private static extern ulong ParseAnimation(byte[] in_buffer, ulong in_len, byte[] out_buffer, ulong out_len);
 
 		static void Main(string[] args)
 		{
-			string in_txt = "MyTestString";
-			byte[] in_buff = Encoding.ASCII.GetBytes(in_txt);
+			byte[] in_buff = File.ReadAllBytes("..\\..\\..\\files\\1.caff");
 			byte[] out_buff = new byte[in_buff.Length];
-			int real_out_len = ParseAnimation(in_buff, in_buff.Length + 1, out_buff, out_buff.Length + 1);
+			int real_out_len = (int)ParseAnimation(in_buff, (ulong)in_buff.Length + 1, out_buff, (ulong)out_buff.Length + 1);
 			string out_txt = Encoding.ASCII.GetString(out_buff, 0, real_out_len);
 			Console.WriteLine(out_txt);
 		}
