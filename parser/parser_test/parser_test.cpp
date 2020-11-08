7aacd93d2dd2605399f474d1d2f4bbad94d46e62
Kókai Kristóf <archetiz@sch.bme.hu>
2020-11-08 16:50:50 +0100

    Optimising input reading speed for better performance

--- parser/parser_test/parser_test.cpp
+++ parser/parser_test/parser_test.cpp
@@ -15,21 +15,28 @@ int main()
     std::string input_path = "../../files/1.caff";
     std::string output_path = "../../output/asd_inout.bmp";
 #if FUZZING
-    // FILE* stream_r, * stream_w;
-    // Change the second (NULL) parameters to file names if you want to redirect stdin/stdout to a file
-    // freopen_s(&stream_r, NULL, "rb", stdin);
-    // freopen_s(&stream_w, NULL, "wb", stdout); Nnemtommiez
-    std::string anim_str{std::istreambuf_iterator<char>(std::cin), std::istreambuf_iterator<char>()};
+    //FILE* stream_r, * stream_w;
+    //Change the second (NULL) parameters to file names if you want to redirect stdin/stdout to a file
+    //freopen_s(&stream_r, NULL, "rb", stdin);
+    //freopen_s(&stream_w, NULL, "wb", stdout);
+    std::streampos fileSize;
+    std::cin.seekg(0, std::ios::end);
+    fileSize = std::cin.tellg();
+    std::cin.seekg(0, std::ios::beg);
+    const int in_len = (int)fileSize + 1;
+    const char* in_buff = new char[in_len];
+    std::cin.read((char*)&in_buff[0], fileSize);
 #else
-    //Reading whole file at once
     std::ifstream anim_in(input_path, std::ios_base::in | std::ios_base::binary);
-    std::string anim_str{std::istreambuf_iterator<char>(anim_in), std::istreambuf_iterator<char>()};
+    std::streampos fileSize;
+    anim_in.seekg(0, std::ios::end);
+    fileSize = anim_in.tellg();
+    anim_in.seekg(0, std::ios::beg);
+    const int in_len = (int)fileSize + 1;
+    const char* in_buff = new char[in_len];
+    anim_in.read((char*)&in_buff[0], fileSize);
 #endif //FUZZING
-
-    //Converting to c-style string to pass for parsing
-    const char *in_buff = anim_str.c_str();
     //Setting output buffers and lengths
-    const int in_len = anim_str.length() + 1;
     const int out_len = in_len;
     char *out_buff = new char[out_len];
     //Passing input for parsing
@@ -49,9 +56,9 @@ int main()
 #endif
     //Writing image, releasing resources
 #if FUZZING
-    // std::cout.write(out_buff, real_out_len);
-    // fclose(stream_r);
-    // fclose(stream_w);
+    std::cout.write(out_buff, real_out_len);
+    //fclose(stream_r);
+    //fclose(stream_w);
 #else
     std::ofstream image_out(output_path, std::ios_base::out | std::ios_base::binary);
     image_out.write(out_buff, real_out_len);
