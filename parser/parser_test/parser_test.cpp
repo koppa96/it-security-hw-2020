3e846dc5dda8b99e81aaf215e114d13216bfcbec
Kókai Kristóf <archetiz@sch.bme.hu>
2020-11-08 19:19:17 +0100

    Fixing input/output piping

--- parser/parser_test/parser_test.cpp
+++ parser/parser_test/parser_test.cpp
@@ -1,11 +1,11 @@
-#define FUZZING 1
+#define INOUT_PIPING 0
 
 #include <string>
 #include <fstream>
 #include "../parser_core/LibApi.h"
 #include "../parser_core/Exceptions.h"
 
-#if FUZZING
+#if INOUT_PIPING
 #include <cstdio>
 #include <iostream>
 #endif
@@ -14,18 +14,15 @@ int main()
 {
     std::string input_path = "../../files/1.caff";
     std::string output_path = "../../output/asd_inout.bmp";
-#if FUZZING
+#if INOUT_PIPING
     //FILE* stream_r, * stream_w;
     //Change the second (NULL) parameters to file names if you want to redirect stdin/stdout to a file
     //freopen_s(&stream_r, NULL, "rb", stdin);
     //freopen_s(&stream_w, NULL, "wb", stdout);
-    std::streampos fileSize;
-    std::cin.seekg(0, std::ios::end);
-    fileSize = std::cin.tellg();
-    std::cin.seekg(0, std::ios::beg);
-    const int in_len = (int)fileSize + 1;
-    const char* in_buff = new char[in_len];
-    std::cin.read((char*)&in_buff[0], fileSize);
+
+    std::string anim_str{ std::istreambuf_iterator<char>(std::cin), std::istreambuf_iterator<char>() };
+    const char* in_buff = anim_str.c_str();
+    const int in_len = anim_str.length() + 1;
 #else
     std::ifstream anim_in(input_path, std::ios_base::in | std::ios_base::binary);
     std::streampos fileSize;
@@ -35,12 +32,12 @@ int main()
     const int in_len = (int)fileSize + 1;
     const char* in_buff = new char[in_len];
     anim_in.read((char*)&in_buff[0], fileSize);
-#endif //FUZZING
+#endif //INOUT_PIPING
     //Setting output buffers and lengths
     const int out_len = in_len;
     char *out_buff = new char[out_len];
     //Passing input for parsing
-#if FUZZING
+#if INOUT_PIPING
     int real_out_len = 0;
     try
     {
@@ -53,9 +50,9 @@ int main()
 
 #else
     const int real_out_len = ParseAnimation(in_buff, in_len, out_buff, out_len);
-#endif
+#endif //INOUT_PIPING
     //Writing image, releasing resources
-#if FUZZING
+#if INOUT_PIPING
     std::cout.write(out_buff, real_out_len);
     //fclose(stream_r);
     //fclose(stream_w);
@@ -64,6 +61,6 @@ int main()
     image_out.write(out_buff, real_out_len);
     anim_in.close();
     image_out.close();
-#endif //FUZZING
+#endif //INOUT_PIPING
     delete[] out_buff;
 }
