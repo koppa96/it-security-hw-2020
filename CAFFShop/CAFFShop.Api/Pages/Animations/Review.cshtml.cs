448ffa534f09798f25f8d55e22a11f5a2dda7f2e
Papp Bence <pappbence96@gmail.com>
2020-11-28 23:19:37 +0100

    Anim review done

---
+++ CAFFShop/CAFFShop.Api/Pages/Animations/Review.cshtml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using CAFFShop.Dal;
+using CAFFShop.Dal.Entities;
+
+namespace CAFFShop.Api.Pages.Animations
+{
+    public class ReviewModel : PageModel
+    {
+        private readonly CAFFShop.Dal.CaffShopContext _context;
+        private readonly CAFFShop.Application.Services.Interfaces.IIdentityService identityService;
+
+        public ReviewModel(CAFFShop.Dal.CaffShopContext context, CAFFShop.Application.Services.Interfaces.IIdentityService identityService)
+        {
+            _context = context;
+            this.identityService = identityService;
+        }
+
+        public IEnumerable<AnimationReviewDto> Animations { get; set; }
+
+        public void OnGet()
+        {
+            LoadAnimations();
+        }
+
+        private void LoadAnimations()
+        {
+            Animations = _context
+                .Animations
+                .Include(a => a.Author)
+                .Where(a => a.ReviewState == ReviewState.Pending)
+                .Select(a => new AnimationReviewDto
+                {
+                    Id = a.Id,
+                    Name = a.Name,
+                    Description = a.Description,
+                    CreationDate = a.CreationTime.ToString("G"),
+                    AuthorName = a.Author.UserName,
+                    Price = a.Price
+                });
+        }
+
+        public async Task<IActionResult> OnPostReject(string id)
+        {
+            if(Guid.TryParse(id, out Guid animId))
+            {
+                var anim = await _context.Animations.FindAsync(animId);
+                anim.ReviewedById = identityService.GetUserId();
+                anim.ReviewState = ReviewState.Rejected;
+                await _context.SaveChangesAsync();
+            }
+            LoadAnimations();
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostApprove(string id)
+        {
+            if (Guid.TryParse(id, out Guid animId))
+            {
+                var anim = await _context.Animations.FindAsync(animId);
+                anim.ReviewedById = identityService.GetUserId();
+                anim.ReviewState = ReviewState.Approved;
+                await _context.SaveChangesAsync();
+            }
+            LoadAnimations();
+            return Page();
+        }
+    }
+    public class AnimationReviewDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string CreationDate { get; set; }
+        public string AuthorName { get; set; }
+        public int Price { get; set; }
+    }
+}
